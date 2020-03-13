f382e4668d21c98c51d36ffe1c3f83763db0a2f0
Clément Renault <renault.cle@gmail.com>
2020-03-13 22:00:33 +0100

    We must support loops of loops ?

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,117 @@
 use std::io::{self, BufReader};
-use std::{str, mem};
+use std::str::FromStr;
+use std::{str, env};
 
-use quick_xml::Reader;
-use quick_xml::events::Event;
-use smallstr::SmallString;
-use smallvec::SmallVec;
+use indexmap::IndexMap;
 use main_error::MainError;
+use quick_xml::events::Event;
+use quick_xml::Reader;
 
-type SmallString64 = SmallString<[u8; 64]>;
-type SmallVec32<T> = SmallVec<[T; 32]>;
 type Scope = Vec<Vec<u8>>;
 
-#[derive(Default)]
-struct Release {
-    id: Option<SmallString64>,
-    album: Option<SmallString64>,
-    artist: Option<SmallString64>,
-    songs: SmallVec32<SmallString64>,
+#[derive(Clone, PartialEq, Eq)]
+struct XPath {
+    path: Vec<Vec<u8>>,
+    attribute: Option<Vec<u8>>,
 }
+
+impl FromStr for XPath {
+    type Err = ();
+    fn from_str(s: &str) -> Result<XPath, Self::Err> {
+        let (path, attribute) = match s.find('@') {
+            Some(p) => {
+                let (left, right) = s.split_at(p);
+                let attribute = right[1..].as_bytes().to_vec();
+                (left, Some(attribute))
+            },
+            None => (s, None),
+        };
+
+        let path = path.split('/')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.as_bytes().to_vec())
+            .collect();
+
+        Ok(XPath { path, attribute })
+    }
+}
+
+// impl Debug for XPath
 
 fn main() -> Result<(), MainError> {
     let xml = BufReader::new(io::stdin());
     let mut reader = Reader::from_reader(xml);
     reader.trim_text(true);
 
-    let mut writer = csv::Writer::from_writer(io::stdout());
-    writer.write_record(&["id", "title", "album", "artist"])?;
+    let mut headers_xpaths = IndexMap::new();
+    for arg in env::args().skip(1) {
+        let mut iter = arg.splitn(2, '=');
+        let header = iter.next().unwrap().trim().to_owned();
+        let xpath = iter.next().unwrap().trim().to_owned();
+        if xpath.is_empty() { panic!("xpath ({}) must be defined", header) }
+        let xpath = XPath::from_str(&xpath).unwrap();
+        headers_xpaths.insert(header, xpath);
+    }
 
-    let mut count = 0;
+    let mut writer = csv::Writer::from_writer(io::stdout());
+    writer.write_record(headers_xpaths.keys())?;
+
     let mut buf = Vec::new();
     let mut scope = Scope::default();
-    let mut release = Release::default();
+    let shortest_scope = headers_xpaths.values().min_by_key(|p| p.path.len()).unwrap();
+    // We reserve the entry strings, that we will clean.
+    let mut completeness = vec![false; headers_xpaths.len()];
+    let mut entry = vec![String::new(); headers_xpaths.len()];
 
     loop {
         match reader.read_event(&mut buf)? {
             Event::Start(e) => {
                 scope.push(e.name().to_owned());
 
-                match e.name() {
-                    b"release" => {
-                        release = Release::default();
-                        if let Some(Ok(attribute)) = e.attributes().find(|a| a.as_ref().map_or(false, |a| a.key == b"id")) {
-                            release.id = Some(SmallString::from_str(str::from_utf8(&attribute.value)?));
+                for (i, xpath) in headers_xpaths.values().enumerate() {
+                    if let Some(attribute) = &xpath.attribute {
+                        if &scope == &xpath.path {
+                            if let Some(Ok(attr)) = e.attributes().find(|a| a.as_ref().map_or(false, |a| a.key == &attribute[..])) {
+                                let text = str::from_utf8(&attr.value).unwrap();
+                                entry[i].clear();
+                                entry[i].push_str(text);
+                                completeness[i] = true;
+                            }
                         }
                     }
-                    _ => (),
                 }
             },
             Event::End(e) => {
+                if &scope == &shortest_scope.path {
+                    completeness.iter_mut().for_each(|x| *x = false);
+                }
+
+                // TODO not sure that is the best solution!
+                for xpath in headers_xpaths.values() {
+                    if &scope == &xpath.path {
+                        if completeness.iter().all(|c| *c) {
+                            writer.write_record(&entry)?;
+                            break;
+                        }
+                    }
+                }
+
                 // only pop if we entered in a scope
                 if scope.last().as_ref().map(AsRef::as_ref) == Some(e.name()) {
                     scope.pop();
                 }
-
-                match e.name() {
-                    b"release" => {
-                        // end of release, we must write the csv line if complete
-                        if let Release { id: Some(id), album: Some(album), artist: Some(artist), songs } = mem::take(&mut release) {
-                            let id: usize = id.parse()?;
-
-                            for (i, title) in songs.into_iter().enumerate().take(100) {
-                                let id = id * 100 + i;
-                                let id = id.to_string();
-
-                                writer.write_record(&[
-                                    id.as_str(),
-                                    title.as_str(),
-                                    album.as_str(),
-                                    artist.as_str()])?;
-                            }
-                        }
-                    },
-                    _ => (),
-                }
             },
 
             Event::Text(e) => {
-                let unescaped = e.unescaped()?;
-                let text = reader.decode(&unescaped)?;
+                for (i, xpath) in headers_xpaths.values().enumerate() {
+                    if xpath.attribute.is_none() && &scope == &xpath.path {
+                        let unescaped = e.unescaped()?;
+                        let text = reader.decode(&unescaped)?;
 
-                if scope == [&b"releases"[..], b"release", b"title"] {
-                    release.album = Some(SmallString64::from_str(text));
-                }
-
-                if scope == [&b"releases"[..], b"release", b"artists", b"artist", b"name"] {
-                    release.artist = Some(SmallString64::from_str(text));
-                }
-
-                if scope == [&b"releases"[..], b"release", b"tracklist", b"track", b"title"] {
-                    count += 1;
-                    if count % 10000 == 0 { eprintln!("{} songs seen", count) }
-                    release.songs.push(SmallString64::from_str(text));
+                        entry[i].clear();
+                        entry[i].push_str(text);
+                        completeness[i] = true;
+                    }
                 }
             },
 
@@ -99,8 +120,6 @@ fn main() -> Result<(), MainError> {
         }
         buf.clear();
     }
-
-    eprintln!("{} songs seen", count);
 
     writer.flush()?;
 
